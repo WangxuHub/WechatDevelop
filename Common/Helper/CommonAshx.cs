c53f8ead75da08197a49b3771a55f23e1263dace
王旭公会 <695166116@qq.com>
2016-02-15 17:36:45 +0800

    13123

---
+++ Common/Helper/CommonAshx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Common.Helper
+{
+    public abstract class CommonAshx:IHttpHandler
+    {
+        public HttpContext conn;
+        public mJsonResult json = new mJsonResult();
+
+        public abstract void ExecPostType(string PostType);
+
+        public void ProcessRequest(HttpContext context)
+        {
+            conn = context;
+
+            string PostType = conn.Request["PostType"];
+            if (string.IsNullOrEmpty(PostType))
+            {
+                json.success = false;
+                json.msg = "无效参数";
+                conn.Response.Write(json.ToJson());
+                conn.Response.End();
+            }
+
+            ExecPostType(PostType);
+
+            if (json != null)
+            {
+                conn.Response.Write(json.ToJson());
+                conn.Response.End();
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 微信开发/Ashx/User.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Common.Helper;
+namespace WebChatDep.Ashx
+{
+    /// <summary>
+    /// User 的摘要说明
+    /// </summary>
+    public class User : CommonAshx
+    {
+        public override void  ExecPostType(string PostType)
+        {
+            switch (PostType)
+            {
+                case "type1":
+                    funct1();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        private void funct1()
+        {
+            json.msg = conn.Request["PostType"];
+        }
+
+    }
+}
